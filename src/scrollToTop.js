21f5b3ef2da3387f1fe961f5a9aea1849dbc30c3
santdas36 <santdas36@gmail.com>
2020-10-22 14:27:33 +0530

    Fix ScrollToTop

--- src/scrollToTop.js
+++ src/scrollToTop.js
@@ -5,7 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    document.querySelector(".app").scroll({
+    window.scroll({
       top: 0,
       left: 0,
       behavior: "smooth",
