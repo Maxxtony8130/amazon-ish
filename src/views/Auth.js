9901a88a43ad47be4e18528ec1e755a68b8f3ad8
santdas36 <santdas36@gmail.com>
2020-10-22 15:40:17 +0530

    auth StateChange

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,18 +1,22 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useQuery } from "../util";
+import { auth } from "../firebase";
 
 function Auth() {
   const query = useQuery();
   const history = useHistory();
 
   useEffect(() => {
-    if (query.get("mode") === "resetPassword") {
-      history.replace(`/password-reset?mode=resetPassword&code=${query.get("oobCode")}`);
-    } else if (query.get("mode") === "verifyEmail") {
-      history.replace(`/login?code=${query.get("oobCode")}`);
-    } else {
-      history.replace("/");
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (query.get("mode") === "resetPassword") {
+        history.replace(`/password-reset?mode=resetPassword&code=${query.get("oobCode")}`);
+      } else if (query.get("mode") === "verifyEmail") {
+        history.replace(`/login?code=${query.get("oobCode")}`);
+      } else {
+        history.replace("/");
+      }
+      unsubscribe();
     }
   }, []);
 
